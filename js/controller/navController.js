44a02f09cceee7474d94a24b71e5547f57a10e56
adamShaleen <adamshaleen@gmail.com>
2016-05-24 18:40:53 -0600

    started working on mobile dropdown navmenu

--- js/controller/navController.js
+++ js/controller/navController.js
@@ -1,8 +1,17 @@
 angular.module('adamShaleen').controller('navController', function($scope, service) {
 
+// Toggle mobile navbar icon from burger to x and back
 $scope.burgerChangeToX = !false;
 $scope.toggleBurger = function() {
     $scope.burgerChangeToX = !$scope.burgerChangeToX;
 };
+
+// Mobile navbar dropdown content shows on click
+$scope.mobileNavMenu = function() {
+    document.getElementById('.nav_dropdown_mobile').classList.toggle('show');
+};
+
+
+
 
 });  // closing controller tag.
