798a0f3e6bf9e9521822be5b59ccb55ab8e1462e
adamShaleen <adamshaleen@gmail.com>
2016-06-14 22:43:28 -0600

    added slick carousel and icons for skills page.  Need to fixe margin error and navbar error.

--- public/js/controller/skillsController.js
+++ public/js/controller/skillsController.js
@@ -1,3 +1,10 @@
 angular.module('adamShaleen').controller('skillsController', function($scope, service) {
 
+    // Carousel of project icons
+    $(document).ready(function() {
+        $('.carousel_icons_mobile').slick({
+            // insert something!
+        });
+    });
+
 });  // closing tag
